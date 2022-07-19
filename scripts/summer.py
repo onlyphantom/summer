294e878cf4ccd8369f118ecec8067a80f4cdaf48
onlyphantom <samuel@outlook.sg>
2022-07-19 14:57:01 +0700

    post recording commit

---
+++ scripts/summer.py
@@ -0,0 +1,31 @@
+from pyodide import JsException
+from pyodide.http import pyfetch
+
+async def wiki_summary(slug):
+
+    try:
+        response = await pyfetch(
+            url=f"https://en.wikipedia.org/api/rest_v1/page/summary/{slug}",
+            method='GET',
+            headers={'Content-Type': 'application/json'}
+        )
+        if response.ok:
+            data = await response.json()
+            return data
+    except JsException:
+        return None
+
+async def get_summary(slug):
+    summary = await wiki_summary(slug)
+    if summary is None:
+        return f"""
+            <h1>No results found 😕.</h1>
+            <p>Wikipedia doesn't have an entry for {slug}. Either that, or Summer is confused. </p>
+            """
+    else:
+        output = f"""
+            <h1>{summary['title']}</h1>
+            <p>{summary['extract_html']}</p>
+            """
+
+        return output
